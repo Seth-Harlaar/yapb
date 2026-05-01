f3755c1f9f9f576bfa06b7d62b552dcf45dca7ad
Seth <sjharlaar@gmail.com>
2026-04-30 21:30:24 -0400

    fix component usage

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,6 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy({ "src/css": "css" });
   eleventyConfig.addPassthroughCopy({ "src/js": "js" });
-  eleventyConfig.addPassthroughCopy({ "src/themes": "themes" });
   eleventyConfig.addPassthroughCopy("src/posts/**/*.{png,jpg,jpeg,gif,webp,svg}");
   eleventyConfig.addPassthroughCopy("src/websites/**/*.{png,jpg,jpeg,gif,webp,svg}");
 
